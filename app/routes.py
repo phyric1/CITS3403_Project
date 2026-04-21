10d846005ea5ec5084b1bd8d7d1f5f65405ea1c7
campbell <campbell.henderson2006@gmail.com>
2026-04-21 11:26:46 +0800

    Added basics of flask server, with routing

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template
+
+from app import app
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "Hello, World!"
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/game")
+def game():
+    return "Game page"
+
+
+@app.route("/leaderboard")
+def leaderboard():
+    return "Leaderboard page"
+
+
+@app.route("/profile")
+def profile():
+    return "Profile page"
