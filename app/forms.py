148bfec5b2dd965e4455ece0508ec9d5837a546b
campbell <campbell.henderson2006@gmail.com>
2026-04-23 18:46:50 +0800

    Added error messages as well as password checking

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=20)])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Sign In")
 
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField("Password", validators=[
+        DataRequired(), Length(min=6),
+        Regexp(
+            r'^(?=.*[A-Za-z])(?=.*\d)',
+            message="Password must contain letters and numbers"
+        )
+    ])
+    confirm_password = PasswordField("Confirm Password", validators=[
+                DataRequired(),
+                EqualTo("password", message="Passwords must match")
+            ])
     submit = SubmitField("Register")
