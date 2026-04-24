ed6ef36928b57ddb75897038fc924f5db094143e
campbell <campbell.henderson2006@gmail.com>
2026-04-24 22:07:40 +0800

    Added inventory page with grid of cards, added basic card template with css styling. Also added placeholder images and data.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, abort
 
 from app import app
 
@@ -32,3 +32,19 @@ def leaderboard():
 @app.route("/profile")
 def profile():
     return "Profile page"
+
+
+@app.route("/profile/<username>/inventory")
+def inventory(username):
+    fake_data = { # Remove when database is implemented
+        "phyric1": [
+            {"name": "Tailwind", "effect": "Move 2 Spaces without triggering enemy behavior", "type": "movement", "rarity": "common", "max": 7},
+            {"name": "Silence Falls", "effect": "Stealth +1", "type": "utility", "rarity": "common", "max": 7},
+            {"name": "Dynamite", "effect": "Deals damage in 5x5 radius, -2 stealth points blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah  blah blah blah blah blah blah blah blah blah", "type": "combat", "rarity": "rare", "max": 3},
+        ]
+    }
+
+    items = fake_data.get(username)
+    if items is None:
+        abort(404)  # user not found
+    return render_template("inventory.html", items=items, username=username)
