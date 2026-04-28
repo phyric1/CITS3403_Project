e6363f1880b3a496bbdc441f9501bc07dbf1e40e
kaojb-bass <jacobpran147@gmail.com>
2026-04-28 20:22:14 +0800

    cleaned up grid variables and methods

--- game_logic.py
+++ game_logic.py
@@ -2,9 +2,7 @@ import random
 from flask import request, jsonify
 from entities import Enemy
 
-#first prioirty - make game object the central hub of logic 
 #second priority - separate dungeon creation from entity spawning
-    #add rooms attributes to grid class
 #third priority - patrol multiple enemies
 
 #1 enemy placed in each empty room
@@ -149,16 +147,16 @@ class Grid():
         GRID_WIDTH = 32
 
         #Room constants
-        MIN_ROOMS = 3
-        MAX_ROOMS = 5
+        ROOM_COUNT = 8
+        MIN_WIDTH = 4
+        MIN_HEIGHT = 4
         MAX_WIDTH = 7
         MAX_HEIGHT = 7
-        MIN_WIDTH = 7
-        MIN_HEIGHT = 7
 
         grid = [[1] * GRID_WIDTH for _ in range(GRID_HEIGHT)]
 
         def overlap(room1, room2):
+            #returns true if rooms overlap
             if room2.x2 < room1.x or room2.x > room1.x2:
                 return True
             if room2.y2 < room1.y or room2.y > room1.y2:
@@ -186,6 +184,7 @@ class Grid():
                         grid[y1][i] = 0
 
         def distance(room1, room2):
+            #returns manhattan distance between 2 rooms
             x1, y1 = room1.center()
             x2, y2 = room2.center()
             return (abs(x1-x2) + abs(y1-y2))
@@ -193,11 +192,11 @@ class Grid():
         rooms = []
         #initialize grid
         success = False
-        for i in range(8):
+        for i in range(ROOM_COUNT):
             success = False
             while success == False:
-                width = random.randint(4, 8)
-                height = random.randint(4, 8)
+                width = random.randint(MIN_WIDTH, MAX_WIDTH)
+                height = random.randint(MIN_HEIGHT, MAX_HEIGHT)
                 x = random.randint(1, GRID_WIDTH - width - 1)
                 y = random.randint(1, GRID_HEIGHT - height - 1)
                 newRoom = Room(width, height, x, y)
