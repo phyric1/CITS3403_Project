214ef1c9638b20dbd1887b305fc783cccc4530c3
Ben Ward <wardguy3ic@gmail.com>
2026-05-11 01:01:17 +0800

    Create a inventory.py for readability

--- app/__init__.py
+++ app/__init__.py
@@ -20,8 +20,10 @@ def create_app():
 
     from app.routes import bp
     from app.inventory import bp as inventory_bp
+    from app.trading import bp as trading_bp
     app.register_blueprint(bp)
     app.register_blueprint(inventory_bp)
+    app.register_blueprint(trading_bp)
     register_cli(app)
 
     return app
