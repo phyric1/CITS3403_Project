08468db032a2586c2a1736b307ff55e32e3fd898
zjhzz <1615890741@qq.com>
2026-05-17 10:07:50 +0800

    Add two small tests

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os.path
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY','dev_secret_key')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
